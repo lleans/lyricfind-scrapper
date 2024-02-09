58f0c22ff0ec3e7e516180a42e057f43e19c2ad1
lleans <ahmadunnail@gmail.com>
2024-02-09 18:44:44 +0700

    🚀 Add project setup and library update

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+requirements = [requirement.strip() for requirement in open('requirements.txt', 'r', encoding='utf-8').readlines()]
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LyricsFindScrapper",
+    version='1.0.0',
+    author="lleans",
+    author_email="",
+    description="LyricsFind scrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lleans/lyricfind-scrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], install_requires=requirements,
+    python_requires='>=3.10',
+)
